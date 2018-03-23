4c75a0c7af75882877a4c0fdc8a7bdb434f1b0b3
Claire D'Ambrosio <claire.dambrosio@ubiquitypress.com@>
2018-03-23 14:00:02 +0000

    add 'license' to primary_terms applied to both Generic Work and Image

--- app/forms/hyrax/generic_work_form.rb
+++ app/forms/hyrax/generic_work_form.rb
@@ -2,19 +2,13 @@
 #  `rails generate curation_concerns:work GenericWork`
 module Hyrax
   class GenericWorkForm < Hyrax::Forms::WorkForm
+    include Hyrax::FormTerms
     self.model_class = ::GenericWork
     include HydraEditor::Form::Permissions
     self.terms += [:resource_type, :rendering_ids]
 
     def secondary_terms
       super - [:rendering_ids]
-    end
-
-    # overrides Hyrax::Forms::WorkForm
-    # to display 'license' in the 'base-terms' div on the user dashboard ‘Add New Work’ description
-    # by getting iterated over in hyrax/app/views/hyrax/base/_form_metadata.html.erb
-    def primary_terms
-      super + [:license]
     end
   end
 end

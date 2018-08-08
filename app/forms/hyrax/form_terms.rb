a763e15865544b641792cfa7b3b92c2d6e95ca51
Claire <claire.dambrosio@ubiquitypress.com>
2018-08-08 16:13:04 +0100

    Reordering of new fields on form

--- app/forms/hyrax/form_terms.rb
+++ app/forms/hyrax/form_terms.rb
@@ -4,9 +4,9 @@ module Hyrax
     # overrides Hyrax::Forms::WorkForm
     # to display 'license' in the 'base-terms' div on the user dashboard "Add New Work" description
     # by getting iterated over in hyrax/app/views/hyrax/base/_form_metadata.html.erb
-    def primary_terms
-      super + [:license]
-    end
+    # def primary_terms
+    #   super + [:license]
+    # end
 
     def secondary_terms
       super - %i[rendering_ids date_created identifier]
--- app/forms/hyrax/journal_article_form.rb
+++ app/forms/hyrax/journal_article_form.rb
@@ -4,9 +4,10 @@ module Hyrax
   class JournalArticleForm < Hyrax::Forms::WorkForm
     include Hyrax::FormTerms
     self.model_class = ::JournalArticle
-    self.terms += %i[resource_type rendering_ids institution org_unit refereed official_link funder fndr_project_ref
-                     add_info date_published date_accepted date_submitted journal_title issue volume
-                     pagination article_num project_name rights_holder doi issn eissn]
-    self.required_fields += %i[journal_title date_published]
+    self.terms += %i[resource_type rendering_ids journal_title doi volume issue pagination issn eissn
+                     article_num date_published date_accepted date_submitted institution org_unit refereed
+                     official_link project_name funder fndr_project_ref add_info rights_holder]
+    self.required_fields += %i[journal_title institution publisher date_published]
+    self.required_fields -= %i[keyword rights_statement]
   end
 end

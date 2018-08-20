7f8d39d94105b4334804e301d59529a054c4ad79
Claire <claire.dambrosio@ubiquitypress.com>
2018-08-20 13:13:45 +0100

    Comply with current design of metadata fields modules

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,8 +2,8 @@
 #  `rails generate hyrax:work Book`
 class Book < ActiveFedora::Base
   include ::Hyrax::WorkBehavior
-  include Hyrax::BasicMetadataDecorator
-  include SharedMetadata
+  include Ubiquity::BasicMetadataDecorator
+  include Ubiquity::SharedMetadata
 
   self.indexer = BookIndexer
   # Change this to restrict which works can be added as a child.

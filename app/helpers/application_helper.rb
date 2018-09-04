5625e902de9b815a4b5175ad669d7638e06b945d
Claire <claire.dambrosio@ubiquitypress.com>
2018-09-04 17:57:51 +0100

    Search result view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,12 @@ module ApplicationHelper
     ["Book", "BookContribution", "ConferenceItem", "Report", "GenericWork"].include? presenter
   end
 
+  ## method below was worked out reading http://jessiekeck.com/customizing-blacklight/metadata_fields/
+  def display_alt_id_fields_in_search(options={})
+    field = JSON.parse(options[:value][0]).map do |alt_id|
+      alt_id["alternate_identifier"] << ", " << alt_id["alternate_identifier_type"]
+    end
+    field.join
+  end
+
 end

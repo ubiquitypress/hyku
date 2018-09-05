f983b3db402267b863659c016fb3729dac82c731
edward <bauchiroad@gmail.com>
2018-09-05 10:05:07 +0100

    title is now single for all work types,  meaning you can't click to add more title fields. Though the form allows only single value, the database still saves it in an array because it is a hyrax default field defined as an array by them

--- app/forms/ubiquity/all_forms_shared_behaviour.rb
+++ app/forms/ubiquity/all_forms_shared_behaviour.rb
@@ -3,35 +3,48 @@ module Ubiquity
     extend ActiveSupport::Concern
 
     included do
-
       attr_accessor :contributor_group, :contributor_name_type, :contributor_type, :contributor_given_name,
-                  :contributor_family_name, :contributor_orcid, :contributor_isni,
-                  :contributor_position, :contributor_organization_name
+                    :contributor_family_name, :contributor_orcid, :contributor_isni,
+                   :contributor_position, :contributor_organization_name
 
       attr_accessor :creator_group, :creator_name_type, :creator_organization_name, :creator_given_name,
                     :creator_family_name, :creator_orcid, :creator_isni,
                     :creator_position
-
     end
 
     class_methods do
+      def multiple?(field)
+        if [:title].include? field.to_sym
+          false
+        else
+          super
+        end
+      end
+
+      def model_attributes(_)
+        attrs = super
+        attrs[:title] = Array(attrs[:title]) if attrs[:title]
+        attrs
+      end
 
       def build_permitted_params
-
         super.tap do |permitted_params|
-
           permitted_params << {contributor_group: [:contributor_organization_name, :contributor_given_name,
             :contributor_family_name, :contributor_name_type, :contributor_orcid, :contributor_isni,
             :contributor_position, :contributor_type
           ]}
 
-           permitted_params << {creator_group: [:creator_organization_name, :creator_given_name,
+          permitted_params << {creator_group: [:creator_organization_name, :creator_given_name,
             :creator_family_name, :creator_name_type, :creator_orcid, :creator_isni,
             :creator_position
           ]}
+        end
       end
+    end  #closes class class_methods
 
-      end
+    #instance methods
+    def title
+      super.first || ""
     end
 
   end

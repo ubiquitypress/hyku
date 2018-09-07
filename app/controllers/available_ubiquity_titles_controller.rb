5c1932346a21ef75ea5b2327cb00ff7fa720254c
edward <bauchiroad@gmail.com>
2018-09-07 16:10:20 +0100

    title validation and notification on if a title is available or taken checked against a specific work type

--- app/controllers/available_ubiquity_titles_controller.rb
+++ app/controllers/available_ubiquity_titles_controller.rb
@@ -1,7 +1,9 @@
 class AvailableUbiquityTitlesController < ApplicationController
   def check
-     data = model.constantize.where(title: title ).first
-     
+    model = params["model_class"]
+    title = params["title"]
+    data = model.constantize.where(title: title).first
+
      if data.present?
        render json: {"data": 'true', "message": "Title is taken"}
      else

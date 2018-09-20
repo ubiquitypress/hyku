d53320f6169321e02ff71dc9f2b1a64fac877513
Claire <claire.dambrosio@ubiquitypress.com>
2018-09-20 17:38:47 +0100

    Avoid rendering fields which are empty

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,10 @@ module ApplicationHelper
   def check_has_editor_fields?(presenter)
     ["Book", "BookContribution", "ConferenceItem", "Report", "GenericWork"].include? presenter
   end
+
+  # called in views/hyrax/base/_attribute_rows to avoid displaying empty content of multi-part fields on works show page
+  def field_has_value?(field)
+    field.delete("[{}]")
+    field.present?
+  end
 end

e53c917ffa4581eea47e35bf2bbcee627b068f8f
Francesco de Virgilio <fradeve@inventati.org>
2018-09-21 13:45:47 +0100

    added Docker entrypoint scripts

---
+++ docker/prod-entrypoint-worker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Configure files requirement when creating a work
+if ! grep -F "config.work_requires_files = false" config/initializers/hyrax.rb
+then
+  sed -i -e "105a\ config.work_requires_files = $WORK_REQUIRES_FILES" config/initializers/hyrax.rb
+fi
+
+bundle exec sidekiq

9fdbc11e9d037affe39d074edc9c43a1ae108a36
Claire <claire.dambrosio@ubiquitypress.com>
2018-09-25 16:18:50 +0100

    Resource Type saves: passed correctly as an array within the model params

--- app/actors/hyrax/actors/conference_item_actor.rb
+++ app/actors/hyrax/actors/conference_item_actor.rb
@@ -1,16 +1,6 @@
 module Hyrax
   module Actors
     class ConferenceItemActor < Hyrax::Actors::BaseActor
-
-      def create(env)
-        puts "PURPLE #{env.attributes.inspect}"
-        super
-      end
-
-      def update(env)
-        puts "PINKO #{env.attributes.inspect}"
-        super
-      end
     end
   end
 end

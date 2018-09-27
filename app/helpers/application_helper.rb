81ec9f2afab9a9012c69c51e21d9d7f0a1e02aef
Claire <claire.dambrosio@ubiquitypress.com>
2018-09-27 14:06:37 +0100

    Override lower level method so the modal also displays RTs correctly

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,14 +15,12 @@ module ApplicationHelper
   end
 
   # override from blacklight/app/helpers/blacklight/facets_helper_behavior.rb
-  def render_facet_partials fields = facet_field_names, options = {}
-    safe_join(facets_from_request(fields).map do |display_facet|
-      if display_facet.name == "resource_type_sim"
-        display_facet.items.each do |i|
-          i[:value] = human_readable_resource_type(i[:value])
-        end
+  def render_facet_limit(display_facet, options = {})
+    if display_facet.name == "resource_type_sim"
+      display_facet.items.each do |i|
+        i[:value] = human_readable_resource_type(i[:value])
       end
-      render_facet_limit(display_facet, options)
-    end.compact, "\n")
+    end
+    super
   end
 end

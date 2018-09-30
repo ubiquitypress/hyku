f953b3781212a782621e2dce5684b3c1ee49adf0
Claire <claire.dambrosio@ubiquitypress.com>
2018-09-30 18:23:39 +0100

    Do not show the breadcrumb trail for non-logged-in users

--- app/controllers/hyrax/shared_behaviors_controller.rb
+++ app/controllers/hyrax/shared_behaviors_controller.rb
@@ -6,7 +6,21 @@ module Hyrax
 
     include Hyku::IIIFManifest
 
-    private
+    # override method from hyrax/app/controllers/concerns/hyrax/breadcrumbs.rb
+    # so we don't display `add_breadcrumb_for_action` when a user is not signed_in
+    def trail_from_referer
+      case request.referer
+      when /catalog/
+        add_breadcrumb I18n.t('hyrax.controls.home'), hyrax.root_path
+        add_breadcrumb I18n.t('hyrax.bread_crumb.search_results'), request.referer
+      else
+        default_trail
+        add_breadcrumb_for_controller if user_signed_in?
+        add_breadcrumb_for_action if user_signed_in?
+      end
+    end
+
+      private
 
       def after_update_response
         download_stats = WorkDownloadStat.find_by(work_uid: presenter.solr_document.id)

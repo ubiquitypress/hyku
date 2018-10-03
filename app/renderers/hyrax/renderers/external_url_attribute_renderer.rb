344895c065ae610fef46f18dd0eee67868229742
Claire <claire.dambrosio@ubiquitypress.com>
2018-10-03 17:48:18 +0100

    Separate multiple URLs with a <br/>

--- app/renderers/hyrax/renderers/external_url_attribute_renderer.rb
+++ app/renderers/hyrax/renderers/external_url_attribute_renderer.rb
@@ -1,20 +1,25 @@
 class ExternalUrlAttributeRenderer < Hyrax::Renderers::AttributeRenderer
+
   def render
     markup = ''
     values.delete("") if values # delete an empty string in array or it would display
     return markup if values.blank? && !options[:include_empty]
-    link = values.is_a?(Array) ? values.join : values
-    li_value(link)
+    return arr_of_li_values(values) if values.is_a?(Array)
+    Rails.logger.info("ExternalUrlAttributeRenderer did not render #{values}")
   end
 
   private
 
-  def li_value(value)
-    markup = %(<tr><th>#{label}</th>\n<td><ul class='tabular'>)
-    attributes = microdata_object_attributes(field).merge(class: "attribute attribute-#{field}")
-    markup << "<li#{html_attributes(attributes)}>"
-    markup << auto_link(value, html: { target: '_blank' })
-    markup << %(</li></ul></td></tr>)
-    markup.html_safe
-  end
+    def arr_of_li_values(value)
+      markup = %(<tr><th>#{label}</th>\n<td><ul class='tabular'>)
+      attributes = microdata_object_attributes(field).merge(class: "attribute attribute-#{field}")
+      markup << "<li#{html_attributes(attributes)}>"
+      links = value.map do |url|
+        auto_link(url, html: { target: '_blank' })
+      end
+      markup << links.join('<br/>')
+      markup << %(</li></ul></td></tr>)
+      markup.html_safe
+    end
+
 end

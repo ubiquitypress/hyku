ea5516a6a83aba10f0f3bdad0d59fa958f61e248
Francesco de Virgilio <fradeve@inventati.org>
2018-10-11 12:45:13 +0100

    fixed config and deployment

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,3 +1,3 @@
 module Hyku
-  VERSION = '1.0.0.beta2'.freeze
+  VERSION = '1.0.0.beta2-1'.freeze
 end
--- docker/set_work_requires_files.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Configure files requirement when creating a work
-if ! grep -F "config.work_requires_files = false" config/initializers/hyrax.rb
-then
-  sed -i -e "105a\ config.work_requires_files = $WORK_REQUIRES_FILES" config/initializers/hyrax.rb
-fi

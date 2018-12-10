72d2e31c16809fdc104aed4808d48c18e293b5ec
edward <bauchiroad@gmail.com>
2018-12-10 12:28:49 +0000

    set work visibility via json importer

--- app/services/ubiquity/json_importer.rb
+++ app/services/ubiquity/json_importer.rb
@@ -1,5 +1,10 @@
-module Ubiquity
 
+#To run from rails console
+#create a json or use the constants set as a examples in queue_import.#!/usr/bin/env ruby -wKU
+# data = sample
+#Ubiquity::JsonImporter.new(data).run
+#
+module Ubiquity
   class JsonImporter
     attr_reader :ubiquity_model_class
 
@@ -12,7 +17,6 @@ module Ubiquity
       @domain = data['domain'] || data[:domain]
 
       @tenant_domain = @tenant + '.' + @domain
-      #@data_hash = HashWithIndifferentAccess.new(data.first)
       @data_hash = HashWithIndifferentAccess.new(data)
       @file = @data_hash[:file]
       @ubiquity_model_class = @data_hash["type"].constantize
@@ -35,7 +39,7 @@ module Ubiquity
       end
       @work_instance.date_modified = Hyrax::TimeService.time_in_utc
       @work_instance.date_uploaded = Hyrax::TimeService.time_in_utc unless @work_instance.date_uploaded.present?
-      @work_instance.visibility = Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PRIVATE unless @work_instance.visibility.present?
+      set_default_work_visibility
       @work_instance.save!
       add_state_to_work
       $stdout.puts "work was successfully created"
@@ -69,8 +73,17 @@ module Ubiquity
       sipity_entity = Sipity::Entity.where(proxy_for_global_id: work.to_global_id.to_s).first
 
       if state.present? && (not sipity_entity.present?)
-        #Sipity::Entity.find_or_create_by(proxy_for_global_id: work.to_global_id.to_s, workflow_state: state, workflow: state.workflow)
         Sipity::Entity.create!(proxy_for_global_id: work.to_global_id.to_s, workflow_state: state, workflow: state.workflow)
+      end
+    end
+
+    def set_default_work_visibility
+      if (['open', 'restricted'].include? @data_hash[:visibility])
+        puts "setting visibility from hash - #{@data_hash[:visibility]}"
+        @work_instance.visibility = @data_hash[:visibility]
+      else
+        puts "setting visibility to private the json importer's default  #{@data_hash[:visibility]}"
+        @work_instance.visibility = Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PRIVATE unless @work_instance.visibility.present?
       end
     end
 
@@ -126,13 +139,11 @@ module Ubiquity
       file_name = File.basename(file.path)
       io = ActionDispatch::Http::UploadedFile.new(tempfile: file, filename: file_name)
       #An activerecord model so return nil when record not found
-      #file.close
       create_hyrax_uploaded_file(io, file_name)
     end
 
     def create_file_from_url(file)
       puts "file url #{file}"
-      #temp_file = open(file)
       temp_file = create_tempfile_from_url(file)
       file_name = File.basename(file)
       io = ActionDispatch::Http::UploadedFile.new(tempfile: temp_file, filename: file_name)
@@ -165,30 +176,27 @@ module Ubiquity
     def create_hyrax_uploaded_file(file_io, file_name)
       AccountElevator.switch!("#{@tenant_domain}")
       fetch_or_create_file ||= Hyrax::UploadedFile.where(file: file_name).first  || Hyrax::UploadedFile.create(file: file_io, user: @user)
-      #@hyrax_uploaded_file = [fetch_or_create_file]
       @hyrax_uploaded_file << fetch_or_create_file
     end
 
     def attach_files
       AccountElevator.switch!("#{@tenant_domain}")
       #create_file
-       create_file_from_array
-
-     #Note that @hyrax_uploaded_file.first.file returns Hyrax::UploadedFileUploader object
-     #Also @hyrax_uploaded_file.first.file.file returns a CarrierWave::SanitizedFile object
-     #and @hyrax_uploaded_file.first.file.file.filename returns the the filename in carrierwave
-     if check_work_has_existing_file_title.present? && @file.present?
-       is_file_in_work = check_work_has_existing_file_title.include? @hyrax_uploaded_file.first.file.file.filename
-     elsif @file.present?
-       is_file_in_work = false
-     else
-       is_file = nil
-     end
-
-     #pass both to AttachFilesToWorkJob
+      create_file_from_array
+      #Note that @hyrax_uploaded_file.first.file returns Hyrax::UploadedFileUploader object
+      #Also @hyrax_uploaded_file.first.file.file returns a CarrierWave::SanitizedFile object
+      #and @hyrax_uploaded_file.first.file.file.filename returns the the filename in carrierwave
+      #
+      if check_work_has_existing_file_title.present? && @file.present?
+        is_file_in_work = check_work_has_existing_file_title.include? @hyrax_uploaded_file.first.file.file.filename
+      elsif @file.present?
+        is_file_in_work = false
+      else
+        is_file = nil
+      end
+      #pass both to AttachFilesToWorkJob
       if @work_instance.present? && @hyrax_uploaded_file.present? && (is_file_in_work != nil) && (is_file_in_work == false)
         $stdout.puts "Attaching files to work"
-
         AttachFilesToWorkJob.perform_later(@work_instance, @hyrax_uploaded_file)
       end
     end

e425c496530d3b32e6d0500a29efe6bd3eb6e996
antonio <antoniodaria@msn.com>
2019-01-14 18:10:24 +0000

    Add more years to date pickers list and rearrenged the views to render a shared partial

--- app/helpers/ubiquity/date_change_helper.rb
+++ app/helpers/ubiquity/date_change_helper.rb
@@ -1,22 +1,5 @@
 module  Ubiquity
   module DateChangeHelper
-    
-    MONTHS = (1..12).to_a.map { |n| n.to_s.rjust(2, '0')}.freeze
-    DAYS = (1..31).to_a.map { |n| n.to_s.rjust(2, '0')}.freeze
-    CURRENT_YEAR_PLUS2 = (Date.today.year) + 2
-    YEARS = ((Date.today.year - 200)..(CURRENT_YEAR_PLUS2)).to_a.reverse.freeze
-
-    def days_array
-      ApplicationHelper::DAYS
-    end
-
-    def months_array
-      ApplicationHelper::MONTHS
-    end
-
-    def years_array
-      ApplicationHelper::YEARS
-    end
 
     def parse_date(date, date_part)
       return nil if date.blank?

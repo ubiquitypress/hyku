1839133a89fe4834a6f32fe074445241b6cb70ba
antonio <antoniodaria@msn.com>
2019-01-29 10:28:11 +0000

    add a 'try' to configuration of local_env.yml to avoid error when no key:value are present

--- config/application.rb
+++ config/application.rb
@@ -69,7 +69,7 @@ module Hyku
     config.before_configuration do
       env_file = File.join(Rails.root, 'config', 'local_env.yml')
       if env_file.present?
-        YAML.load(File.open(env_file)).each do |key, value|
+        YAML.load(File.open(env_file)).try(:each) do |key, value|
           ENV[key.to_s] = value
         end if File.exists?(env_file)
       end

7b2651c22cfe32f3db8d165383c2408ebaac9197
antonio <antoniodaria@msn.com>
2019-03-14 22:25:03 +0000

    add link back to search result for shared search

--- app/controllers/concerns/ubiquity/breadcrumb_override.rb
+++ app/controllers/concerns/ubiquity/breadcrumb_override.rb
@@ -15,11 +15,14 @@ module Ubiquity
       when /catalog/
         #add_breadcrumb I18n.t('hyrax.controls.home'), hyrax.root_path
         add_breadcrumb I18n.t('hyrax.bread_crumb.search_results'), request.referer
+      when //
+        add_breadcrumb I18n.t('hyrax.bread_crumb.search_results'), request.referer
       else
         default_trail
         add_breadcrumb_for_controller if user_signed_in?
         add_breadcrumb_for_action if user_signed_in?
       end
     end
+
   end
 end

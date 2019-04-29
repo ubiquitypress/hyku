dc15aa4be6a5e67e12666d8d5ed201b0c4a7ba11
ashikajith <ashikajith@gmail.com>
2019-04-29 14:31:30 +0100

    Added rake taskt to update the file_status column in the UploadedFile model

--- app/controllers/concerns/ubiquity/uploads_controller_override.rb
+++ app/controllers/concerns/ubiquity/uploads_controller_override.rb
@@ -5,10 +5,10 @@ module Ubiquity
     def create
       # Replace all the non characters to underscore except - and  .
       file_name = params[:files].first.original_filename.gsub(/[^a-zA-Z0-9\-.]/, '_')
-      @upload = Hyrax::UploadedFile.where(file: file_name, user_id: current_user.id, file_status: 0).first
+      @upload = Hyrax::UploadedFile.where(file: file_name, user_id: current_user.id, file_status: 'pending').first
       if @upload.nil?
         @upload = Hyrax::UploadedFile.create(file: params[:files].first, user_id: current_user.id)
-      elsif @upload.file_status.zero?
+      elsif @upload.file_status == 'pending'
         path = @upload.file.path
         File.open(path, "ab") { |f| f.write(params[:files].first.read) }
       end

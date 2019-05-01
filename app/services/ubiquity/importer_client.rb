c190e2dd6395405aaf968c271f15edf1cae008d3
ashikajith <ashikajith@gmail.com>
2019-05-01 12:11:36 +0100

    Added method for the s3 url loading for the corresponding work

---
+++ app/services/ubiquity/importer_client.rb
@@ -0,0 +1,28 @@
+module Ubiquity
+  class ImporterClient
+    # Api call wrapper class for setting up the s3 url to each file set records
+    # Called in show page and the file details page of the evert works created by user
+    include HTTParty
+    format :json
+    base_uri 'https://importer.repo-test.ubiquity.press'
+
+    attr_accessor :file_url_hash
+
+    def initialize(response)
+      if response
+        @file_url_hash = Hash[response['works'].map { |ele| [ele['uuid'], ele['providers']['S3Storage']['link']] }]
+      else
+        @file_url_hash = {}
+      end
+    end
+
+    def self.get_s3_url(uuid)
+      response = get("/api/entry/#{uuid}", base_uri: base_uri)
+      if response.success?
+        new(response)
+      else
+        new(nil)
+      end
+    end
+  end
+end

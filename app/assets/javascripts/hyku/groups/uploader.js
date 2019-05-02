51396fdcd719f79dc27e5fc860c08280ee68f028
antonio <antoniodaria@msn.com>
2019-05-02 17:44:17 +0100

    add modal to render validtion messages when date published is blank

--- app/assets/javascripts/hyku/groups/uploader.js
+++ app/assets/javascripts/hyku/groups/uploader.js
@@ -16,4 +16,24 @@ Blacklight.onLoad(function() {
       }
     });
   });
+  console.log('thisis loading');
+  $('#with_files_submit').on('click', function(e) {
+    var publicationYear = $('#date_year').val();
+    var publicationMonth = $('#date_month').val();
+    var publicationDay = $('#date_day').val();
+    var date = new Date(publicationYear + '-' + publicationMonth + '-' + publicationDay)
+    if (date == 'Valid Date') //check for public visibility and embargo {
+      var msg = 'Message in the case of publication year is Not blank'
+      $('#modal_button_save').attr('disabled', false);
+    }
+    else {
+      var msg = 'hi this is condition when the published Date  is blank'
+      $('#modal_button_save').attr('disabled', true);
+    }
+    $('#modal_message').text(msg)
+    $('#myModal').modal('show');
+
+    console.log('preventing Submitted');
+    e.preventDefault();
+  });
 });
---
+++ app/assets/javascripts/ubiquity/external_services_modal.js
@@ -0,0 +1,9 @@
+// $(document).on('turbolinks:load',function(ev) {
+//   console.log('thisis loading');
+//     $('#with_files_submit').on('click', function(ev) {
+//       console.log('Submitted');
+//       $('#myModal').modal('show');
+//       console.log('preventing Submitted');
+//       ev.preventDefault();
+//   });
+// });

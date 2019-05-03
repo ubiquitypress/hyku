b1825f9bfa6c9e539e938394904e630fff73a89a
antonio <antoniodaria@msn.com>
2019-05-03 16:37:52 +0100

    add styling to modal

--- app/assets/javascripts/hyku/groups/uploader.js
+++ app/assets/javascripts/hyku/groups/uploader.js
@@ -18,22 +18,32 @@ Blacklight.onLoad(function() {
   });
   console.log('thisis loading');
   $('#with_files_submit').on('click', function(e) {
-    var publicationYear = $('#date_year').val();
-    var publicationMonth = $('#date_month').val();
-    var publicationDay = $('#date_day').val();
+    var publicationYear = $('.ubiquity-date-published-year').val();
+    var publicationMonth = $('.ubiquity-date-published-month').val();
+    var publicationDay = $('.ubiquity-date-published-day').val();
+    var visibility = $('.set-access-controls ul.visibility li.radio input:checked').val();
+    var doiOptions = $('ul.doi_option_list input:checked').val();
     var date = new Date(publicationYear + '-' + publicationMonth + '-' + publicationDay);
-    if (date instanceof Date) {
-      var msg = 'Please note that a DOI will be minted at DataCite if you continue (or an updated record will be sent to DataCite) - if your work is under embargo, then the record will be sent to DataCite on release of the embargo'
-      $('#modal_button_save').attr('disabled', false);
-    }
-    else {
+
+    if (date == 'Invalid Date') {
       var msg = 'Please go back and add a publication year, as this is required by DataCite'
       $('#modal_button_save').attr('disabled', true);
     }
+    else {
+      var msg = 'Please note that a DOI will be minted at DataCite if you continue (or an updated record will be sent to DataCite) - if your work is under embargo, then the record will be sent to DataCite on release of the embargo'
+      $('#modal_button_save').attr('disabled', false);
+    }
     $('#modal_message').text(msg)
-    $('#myModal').modal('show');
 
+    if ((visibility == "open") || (visibility == "embargo") && (doiOptions == "Mint DOI:Registered") || (doiOptions == "Mint DOI:Findable") ) {
+      console.log('MODALL');
+
+      $('#doi-options-modal').modal('show');
+      e.preventDefault();
+    }
+
+    // $('#doi-options-modal').modal('show');
     console.log('preventing Submitted');
-    e.preventDefault();
+
   });
 });

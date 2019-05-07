a7a7bcd5fbe9853e07e87b683c87a30646ca5c9d
antonio <antoniodaria@msn.com>
2019-05-07 10:51:25 +0100

    Add condition for different modal message in case of editing a record

--- app/assets/javascripts/hyku/groups/uploader.js
+++ app/assets/javascripts/hyku/groups/uploader.js
@@ -24,26 +24,28 @@ Blacklight.onLoad(function() {
     var visibility = $('.set-access-controls ul.visibility li.radio input:checked').val();
     var doiOptions = $('ul.doi_option_list input:checked').val();
     var draftDoi = $(".ubiquity-draft-doi").val()
+    var doi = $(".ubiquity-doi").val()
     var date = new Date(publicationYear + '-' + publicationMonth + '-' + publicationDay);
 
     if (date == 'Invalid Date') {
       var msg = 'Please go back and add a publication year, as this is required by DataCite'
       $('#modal_button_save').attr('disabled', true);
     }
-    else if (draftDoi !== ''){
-      var msg = 'PPPPPPPlease note that a DOI will be minted at DataCite if you continue or, if your work is under embargo, then the record will be sent to DataCite on release of the embargo'
+    else if (doi !== ''){
+      var msg = 'Please note an updated record will be sent to DataCite, if you continue'
       $('#modal_button_save').attr('disabled', false);
     }
-    else {
+    else if (draftDoi !== ''){
       var msg = 'Please note that a DOI will be minted at DataCite if you continue or, if your work is under embargo, then the record will be sent to DataCite on release of the embargo'
       $('#modal_button_save').attr('disabled', false);
     }
+
 
     $('#modal_message').text(msg)
     var visibilityCheck = (visibility == "open" || visibility == "embargo")
     var doiOptionsCheck = (doiOptions == "Mint DOI:Registered" || doiOptions == "Mint DOI:Findable")
 
-
+//conditions to be met to show modal window
       if (visibilityCheck && doiOptionsCheck) {
         $('#doi-options-modal').modal('show');
         e.preventDefault();

2e8496082d13bf785a49d4c2eb868173570088dd
antonio <antoniodaria@msn.com>
2019-05-13 16:30:12 +0100

    allow universal viewer to load manifest for private file by skipping authorization fo revery model

--- app/controllers/hyrax/articles_controller.rb
+++ app/controllers/hyrax/articles_controller.rb
@@ -1,7 +1,7 @@
 module Hyrax
 
   class ArticlesController < SharedBehaviorsController
-
+    skip_load_and_authorize_resource only: [:manifest, :show]
     self.curation_concern_type = ::Article
 
   end
--- app/controllers/hyrax/images_controller.rb
+++ app/controllers/hyrax/images_controller.rb
@@ -1,12 +1,10 @@
 module Hyrax
   class ImagesController < SharedBehaviorsController
+      skip_load_and_authorize_resource only: [:manifest, :show]
     self.curation_concern_type = ::Image
 
     # Use this line if you want to use a custom presenter
     # self.show_presenter = Hyrax::ImagePresenter
 
-    # Instead we include IIIFManifest which uses the manifest-enabled show
-    # presenter
-    include Hyku::IIIFManifest
   end
 end

056d51c61e5aa2c346dcfdc988e4a9e22c486728
antonio <antoniodaria@msn.com>
2019-05-14 16:03:56 +0100

    fix to allow universal viewer to work on sandbox

--- app/helpers/ubiquity/file_display_helpers.rb
+++ app/helpers/ubiquity/file_display_helpers.rb
@@ -14,7 +14,7 @@ module Ubiquity
          '<span class="center-block fa fa-file-o fa-5x hidden-xs file_listing_thumbnail" style="color:grey"></span>'
       elsif ((check_file_is_restricted?(file_set_presenter) == nil) && (file_set_presenter.lease_expiration_date.present?) && (file_set_presenter.embargo_release_date.present?) )
         '<span class="center-block fa fa-file-o fa-5x hidden-xs file_listing_thumbnail" style="color:grey"></span>'
-      elsif ((check_file_is_restricted?(file_set_presenter) == true) || (not file_set_presenter.lease_expiration_date.present?) && (not file_set_presenter.embargo_release_date.present?) && ( file_set_presenter.visibility == "open") )
+      elsif ((check_file_is_restricted?(file_set_presenter) == true) || (not file_set_presenter.lease_expiration_date.present?) && (not file_set_presenter.embargo_release_date.present?) && ( file_set_presenter.solr_document['visibility_ssi'] == "open") )
         #displays for logged out users on files without embargo/lease
         #also displays for logged_in users on files with embargo/lease
         render_thumbnail_tag(file_set_presenter)

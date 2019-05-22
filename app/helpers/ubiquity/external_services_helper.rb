3c23d3b64db510f5a5d286256caf37d84a0773a7
antonio <antoniodaria@msn.com>
2019-05-22 15:30:15 +0100

    Fix draft-doi display flow

--- app/helpers/ubiquity/external_services_helper.rb
+++ app/helpers/ubiquity/external_services_helper.rb
@@ -4,4 +4,11 @@ module Ubiquity::ExternalServicesHelper
     get_tenant_settings_hash(original_url)['ENABLE_DOI'] == 'true'  &&  feature_list['doi_option'] == 'true' && get_tenant_settings_hash(original_url)["datacite_prefix"].present?
   end
 
+  def display_doi_text(work_object)
+    if ['Do not mint DOI', 'Keep DOI as Draft'].include?(work_object.object.model.doi_options)
+      'Draft DOI'
+    else
+      'DOI'
+    end
+  end
 end

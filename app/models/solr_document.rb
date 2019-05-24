c0d0ba2ffb6a5e56db98695290e86d4d4bdebd0a
antonio <antoniodaria@msn.com>
2019-05-24 14:08:03 +0100

    add alternative_title to work show page

--- app/models/solr_document.rb
+++ app/models/solr_document.rb
@@ -66,4 +66,5 @@ class SolrDocument
   attribute :editor, Solr::Array, solr_name('editor')
   attribute :creator_search, Solr::Array, solr_name('creator_search')
   attribute :library_of_congress_classification, Solr::Array, solr_name('library_of_congress_classification')
+  attribute :alternative_title, Solr::Array, solr_name('alternative_title')
 end

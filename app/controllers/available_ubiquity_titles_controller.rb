37dfa67409c2f2039166a70de8b65732b308129a
antonio <antoniodaria@msn.com>
2019-05-24 17:22:33 +0100

    title availability message changes dinamically

--- app/controllers/available_ubiquity_titles_controller.rb
+++ app/controllers/available_ubiquity_titles_controller.rb
@@ -6,14 +6,14 @@ class AvailableUbiquityTitlesController < ApplicationController
     title_ary.each do |title|
       data = params["model_class"].constantize.where(title: title).first
       if data.present?
-        title_present_ary << title
+        title_present_ary << " #{title}"
         find_title = true
       end
     end
     if find_title
-      render json: { "data": 'true', "message": "Following Title/Titles are taken: ", "title_list": title_present_ary }
+      render json: { "data": 'true', "message": title_unavailable_message_based_on_count(title_present_ary), "title_list": title_present_ary}
     else
-      render json: { "data": 'false', "message": "Title is available" }
+      render json: { "data": 'false', "message": title_available_message_based_on_count(title_ary) }
     end
   end
 
@@ -23,3 +23,23 @@ class AvailableUbiquityTitlesController < ApplicationController
     render json: { 'data': datacite.data }
   end
 end
+
+
+private
+
+  def title_unavailable_message_based_on_count(title_ary)
+    if title_ary.count > 1
+      'The Following Titles are being used: '
+    else
+      'This Title is being used: '
+    end
+  end
+
+  def title_available_message_based_on_count(title_ary)
+    title_ary.reject!(&:blank?)
+    if title_ary.count > 1
+      'The Titles are available'
+    else
+      'This Title is available'
+    end
+  end

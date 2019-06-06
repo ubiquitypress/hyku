8312eed2d77df67bb6076c272e4fff21b97ddda8
ashikajith <ashikajith@gmail.com>
2019-06-06 12:37:22 +0100

    Added expiry service model and the rake task to populate the records for embargo service

--- app/jobs/ubiquity_work_expiry_job.rb
+++ app/jobs/ubiquity_work_expiry_job.rb
@@ -1,52 +1,107 @@
 class UbiquityWorkExpiryJob < ActiveJob::Base
   queue_as :expiry_service
-  #adds get_visibility_after_expiration and compare_visibility_after_expiration?
+  # adds get_visibility_after_expiration and compare_visibility_after_expiration?
   include Ubiquity::ExpiryUtil
-  def perform(work_id, work_class,  type, tenant)
+  def perform(work_id, tenant, work_type)
     AccountElevator.switch!("#{tenant}")
-    @works = fetch_work(work_id, work_class)
-    if @works.present? && type == "embargo"
-      auto_expire_embargo
-    elsif @works.present? && type == 'lease'
-      auto_expire_lease
+    if work_type == 'work'
+      perform_work_expiry(work_id)
+    else
+      perform_file_expiry(work_id)
+    end
+  end
+
+  def perform_work_expiry(work_id)
+    @work = ActiveFedora::Base.find(work_id)
+    if @work
+      if @work.under_embargo?
+        auto_expire_embargo
+      elsif @work.active_lease?
+        auto_expire_lease
+      end
+      update_expired_file_sets
+      unless @work.under_embargo? || @work.active_lease?
+        WorkExpiryService.where(work_id: work_id).first.destroy
+      end
+    else
+      WorkExpiryService.where(work_id: work_id).first.destroy
+    end
+  end
+
+  def perform_file_expiry(work_id)
+    file_set = ActiveFedora::Base.find(work_id)
+    if file_set
+      if file_set.under_embargo?
+        auto_expire_file_embargo file_set
+      elsif file_set.active_lease?
+        auto_expire_file_lease file_set
+      end
+      unless file_set.under_embargo? ||file_set.active_lease?
+        WorkExpiryService.where(work_id: work_id).first.destroy
+      end
+    else
+      WorkExpiryService.where(work_id: work_id).first.destroy
     end
   end
 
   private
 
-  def fetch_work(work_id, work_class)
-    if work_class.present? && work_id.present?
-      work_class.constantize.where(id: work_id)
-    end
-  end
-
-  def auto_expire_embargo
-    @works.each do |work|
-      #compare_visibility_after_expiration?(work, 'embargo')
-      puts "expiring work embargo #{work}"
-      if work.embargo_release_date.present?
-        puts "embargo work with date #{work}"
-        Hyrax::Actors::EmbargoActor.new(work).destroy
-      elsif (not work.embargo_release_date.present?) && compare_visibility_after_expiration?(work, 'embargo')
-        puts "work with no embargo date #{work}"
-        work.visibility = get_visibility_after_expiration(work, 'embargo')
-        work.save if work.visibility.present?
+    def auto_expire_embargo
+      # compare_visibility_after_expiration?(work, 'embargo')
+      puts "expiring work embargo #{@work}"
+      if @work.embargo_release_date.present?
+        puts "embargo work with date #{@work}"
+        Hyrax::Actors::EmbargoActor.new(@work).destroy
+      elsif compare_visibility_after_expiration?(@work, 'embargo')
+        puts "work with no embargo date #{@work}"
+        @work.visibility = get_visibility_after_expiration(@work, 'embargo')
+        @work.save if @work.visibility.present?
       end
     end
-  end
 
-  def auto_expire_lease
-    @works.each do |work|
-      puts "expiring work lease #{work}"
-      if (work.lease_expiration_date.present?)
-        puts "lease work with date #{work}"
-        Hyrax::Actors::LeaseActor.new(work).destroy
-      elsif (not work.lease_expiration_date.present?) && compare_visibility_after_expiration?(work, 'lease')
+    def auto_expire_lease
+      puts "expiring work lease #{@work}"
+      if @work.lease_expiration_date.present?
+        puts "lease work with date #{@work}"
+        Hyrax::Actors::LeaseActor.new(@work).destroy
+      elsif compare_visibility_after_expiration?(@work, 'lease')
         puts "work with no lease date #{work}"
-        work.visibility = get_visibility_after_expiration(work, 'lease')
-        work.save if work.visibility.present?
+        @work.visibility = get_visibility_after_expiration(@work, 'lease')
+        @work.save if @work.visibility.present?
       end
     end
-  end
 
+    def update_expired_file_sets
+      @work.file_sets.each do |file_set|
+        if file_set.under_embargo?
+          auto_expire_file_embargo file_set
+        elsif file_set.active_lease?
+          auto_expire_file_lease file_set
+        end
+      end
+    end
+
+    def auto_expire_file_embargo(file_set)
+      puts "expiring fileset embargo #{file_set}"
+      if file_set.embargo_release_date.present?
+        puts "embargo file_set with date #{file_set}"
+        Ubiquity::FileEmbargoActor.new(file_set).destroy
+      elsif compare_visibility_after_expiration?(file_set, 'embargo')
+        puts "file_set with no embargo date #{file_set}"
+        file_set.visibility = get_visibility_after_expiration(file_set, 'embargo')
+        file_set.save if file_set.visibility.present?
+      end
+    end
+
+    def auto_expire_file_lease(file_set)
+      puts "expiring fileset lease #{file_set}"
+      if (file_set.lease_expiration_date.present?)
+        puts "lease file_set with date #{file_set}"
+        Ubiquity::FileLeaseActor.new(file_set).destroy
+      elsif compare_visibility_after_expiration?(file_set, 'lease')
+        puts "file_set with no lease date #{file_set}"
+        file_set.visibility = get_visibility_after_expiration(file_set, 'lease')
+        file_set.save if file_set.visibility.present?
+      end
+    end
 end

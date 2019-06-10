5ba7ae9710df65ffe446872402156d5cd3d9c78c
ashikajith <ashikajith@gmail.com>
2019-06-10 12:02:54 +0100

    Added date time condition in the work service to update the records

--- app/services/ubiquity/expiry_service.rb
+++ app/services/ubiquity/expiry_service.rb
@@ -4,10 +4,10 @@ module Ubiquity
     include Ubiquity::ExpiryUtil
 
     def release_expired_works
-      WorkExpiryService.work_records.each do |work|
+      WorkExpiryService.work_records.less_than_current_time.each do |work|
         UbiquityWorkExpiryJob.perform_later(work.work_id, work.tenant_name, 'work')
       end
-      WorkExpiryService.file_set_records.each do |work|
+      WorkExpiryService.file_set_records.less_than_current_time.each do |work|
         UbiquityWorkExpiryJob.perform_later(work.work_id, work.tenant_name, 'file')
       end
     end

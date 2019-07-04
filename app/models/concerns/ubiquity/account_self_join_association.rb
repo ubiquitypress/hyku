a7e67f79749b1683c6720d3451305cff068a4458
edward <bauchiroad@gmail.com>
2019-07-04 12:06:02 +0100

    we can know use account settings to to set the value of false to exempt a tenant's record from being indexed to shared-search

--- app/models/concerns/ubiquity/account_self_join_association.rb
+++ app/models/concerns/ubiquity/account_self_join_association.rb
@@ -1,12 +1,33 @@
 module Ubiquity
   module AccountSelfJoinAssociation
     extend ActiveSupport::Concern
+
     included do
+
       has_many :children, class_name: "Account", foreign_key: "parent_id", dependent: :destroy, inverse_of: :parent
       belongs_to :parent, class_name: "Account", inverse_of: :parent, foreign_key: "parent_id", optional: true
 
       store_accessor :data, :is_parent
-      store_accessor :settings, :contact_email, :weekly_email_list, :monthly_email_list, :yearly_email_list
+      store_accessor :settings, :contact_email, :weekly_email_list, :monthly_email_list, :yearly_email_list,
+                                :include_in_shared_search
+
+      after_initialize :set_include_in_shared_search_for_live_sites, :set_include_in_shared_search_for_demo_sites
+
     end
+
+    private
+
+    def set_include_in_shared_search_for_live_sites
+      if cname.present? && !cname.include?('demo')
+        self.settings['include_in_shared_search'] = 'true' if settings['include_in_shared_search'].blank?
+      end
+    end
+
+    def set_include_in_shared_search_for_demo_sites
+      if cname.present? && 'demo'.in?(self.cname)
+        self.settings['include_in_shared_search'] = 'false' if settings['include_in_shared_search'].blank?
+      end
+    end
+
   end
 end
--- app/models/concerns/ubiquity/update_shared_index.rb
+++ app/models/concerns/ubiquity/update_shared_index.rb
@@ -4,7 +4,7 @@ module Ubiquity
 
     included do
       after_save :add_record_to_index, on: [:create, :update]
-      #before_destroy :remove_record_from_index
+      before_destroy :remove_record_from_index
 
       before_save :get_collection_child_records
       before_destroy :get_collection_child_records
@@ -16,22 +16,27 @@ module Ubiquity
     private
 
     def add_record_to_index
-      if parent_tenant.present? && !self.account_cname.include?('demo')
-        Ubiquity::SharedIndexSolrServiceWrapper.new(self.to_solr, 'add', parent_tenant, get_file_sets).update
+      shared_search_check =  get_account_tenant.settings['include_in_shared_search']
+      if parent_tenant.present? && shared_search_check == 'true'
+        Ubiquity::SharedIndexSolrServiceWrapper.new(self.to_solr, 'add', parent_tenant, get_file_sets, shared_search_check).update
         #if you switch the tenant back to the one that owns the work, you will get a blacklight error rendering show
         AccountElevator.switch!(self.account_cname)
       end
     end
 
     def remove_record_from_index
-      if parent_tenant.present? && !self.account_cname.include?('demo')
+      if parent_tenant.present?
           Ubiquity::SharedIndexSolrServiceWrapper.new(self.to_solr, 'remove', parent_tenant).update
           AccountElevator.switch!(self.account_cname)
       end
     end
 
+    def get_account_tenant
+      @account ||=  Account.where(cname: self.account_cname).first
+    end
+
     def parent_tenant
-      account = Account.where(cname: self.account_cname).first
+      account =  get_account_tenant
       if account.present? && account.parent.present?
         parent = account.parent
         parent.cname

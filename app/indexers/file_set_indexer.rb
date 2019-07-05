fe86fd1009180f0d926111a041006f9c40075151
edward <bauchiroad@gmail.com>
2019-07-05 13:35:28 +0100

    rescue when to_solr fails in file_set indexer when running  file_availability rake task

--- app/indexers/file_set_indexer.rb
+++ app/indexers/file_set_indexer.rb
@@ -1,8 +1,14 @@
 class FileSetIndexer < Hyrax::FileSetIndexer
   self.thumbnail_path_service = IIIFWorkThumbnailPathService
   def generate_solr_document
+
     super.tap do |solr_doc|
       solr_doc['hasFormat_ssim'] = object.rendering_ids
     end
+
+    rescue Ldp::HttpErro => exception
+     puts "exception is: #{exception.inspect}"
+     puts "calling to_solr on fileset failed #{object.inspect}"
+    
   end
 end

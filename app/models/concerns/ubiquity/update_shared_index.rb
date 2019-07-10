b6ff43d29e7fc3584fe9016288969a86caea4d20
edward <bauchiroad@gmail.com>
2019-07-10 08:52:40 +0100

    deleteing a work with a file should no longer crash the app

--- app/models/concerns/ubiquity/update_shared_index.rb
+++ app/models/concerns/ubiquity/update_shared_index.rb
@@ -3,11 +3,21 @@ module Ubiquity
     extend ActiveSupport::Concern
 
     included do
+      #adds a work or file to shared index.
       after_save :add_record_to_index, on: [:create, :update]
-      after_save :get_file_sets, on: [:create, :update]
 
+      #removes a work or a file from shared_search
       before_destroy :remove_record_from_index
 
+      #adds the file_sets in a work to shared_search index after a work has saved
+      after_save :index_file_sets_in_work,  on: [:create, :update]
+
+      #get all the the filesets in a work before the work is destroyed
+      before_destroy :get_fileset_ids
+      #remove the files in get_fileset_ids from shared_search index
+      after_destroy :remove_file_sets_in_work_from_index
+
+      #get the works in a collection, for subsequest removel from or addition into shared_search index
       before_save :get_collection_child_records
       before_destroy :get_collection_child_records
 
@@ -16,22 +26,6 @@ module Ubiquity
     end
 
     private
-
-    def add_record_to_index
-      shared_search_check =  get_account_tenant && get_account_tenant.settings['include_in_shared_search']
-      if parent_tenant.present? && shared_search_check == 'true'
-        Ubiquity::SharedIndexSolrServiceWrapper.new(self.to_solr, 'add', parent_tenant, shared_search_check).update
-        #if you switch the tenant back to the one that owns the work, you will get a blacklight error rendering show
-        AccountElevator.switch!(self.account_cname)
-      end
-    end
-
-    def remove_record_from_index
-      if parent_tenant.present?
-          Ubiquity::SharedIndexSolrServiceWrapper.new(self.to_solr, 'remove', parent_tenant).update
-          AccountElevator.switch!(self.account_cname)
-      end
-    end
 
     def get_account_tenant
       if self.account_cname.present?
@@ -47,12 +41,44 @@ module Ubiquity
       end
     end
 
-    def get_file_sets
+    def add_record_to_index
       shared_search_check =  get_account_tenant && get_account_tenant.settings['include_in_shared_search']
+      if parent_tenant.present? && shared_search_check == 'true'
+        puts "adding record to shared-search #{self.class} - title is #{self.try(:title)} - with id #{self.id}"
+        Ubiquity::SharedIndexSolrServiceWrapper.new(self.to_solr, 'add', parent_tenant, shared_search_check).update
+        #if you switch the tenant back to the one that owns the work, you will get a blacklight error rendering show
+        AccountElevator.switch!(self.account_cname)
+      end
+    end
 
+    def remove_record_from_index
+      if parent_tenant.present?
+          puts "removing record from shared-search #{self.class} - title is #{self.try(:title)} - with id #{self.id}"
+          Ubiquity::SharedIndexSolrServiceWrapper.new(self.id, 'remove', parent_tenant).update
+          AccountElevator.switch!(self.account_cname)
+      end
+    end
+
+    def index_file_sets_in_work
+      shared_search_check =  get_account_tenant && get_account_tenant.settings['include_in_shared_search']
       if self.class != FileSet && self.try(:file_sets).present? && parent_tenant.present? && shared_search_check == 'true'
-        file_sets_solr =  self.file_sets.map(&:to_solr)
+        file_sets_solr = self.file_sets.map(&:to_solr)
+        puts "adding work fileset_ids to shared-search #{self.class} - title is #{self.try(:title)} - with id #{self.id}"
         Ubiquity::SharedIndexSolrServiceWrapper.new(file_sets_solr, 'add', parent_tenant, shared_search_check).update
+        AccountElevator.switch!(self.account_cname)
+      end
+    end
+
+    def get_fileset_ids
+      if self.class != FileSet && self.try(:file_sets).present?
+        self.file_sets.map(&:id)
+      end
+    end
+
+    def remove_file_sets_in_work_from_index
+      if get_fileset_ids.present? && parent_tenant.present?
+        puts "removing work fileset_ids to shared-search #{self.class} - title is #{self.try(:title)} - with id #{self.id}"
+        Ubiquity::SharedIndexSolrServiceWrapper.new(get_fileset_ids, 'remove', parent_tenant).update
         AccountElevator.switch!(self.account_cname)
       end
     end
--- app/services/ubiquity/shared_index_solr_service_wrapper.rb
+++ app/services/ubiquity/shared_index_solr_service_wrapper.rb
@@ -1,67 +1,58 @@
 module Ubiquity
   class SharedIndexSolrServiceWrapper
-    attr_accessor :solr_document, :action_type, :tenant_cname, :file_sets, :add_to_in_shared_search
+    attr_accessor :data, :action_type, :tenant_cname, :add_to_in_shared_search
 
-    def initialize(solr_document, action_type, tenant_cname, add_to_in_shared_search = nil, file_sets = nil)
-      @solr_document = solr_document
+    def initialize(data, action_type, tenant_cname, add_to_in_shared_search = nil)
+      @data = data
       @action_type = action_type
-      @file_sets = file_sets
       @tenant_cname = tenant_cname
       @add_to_in_shared_search = add_to_in_shared_search
     end
 
     def update
       if action_type == "add" && add_to_in_shared_search == 'true'
+        puts "olly #{data.inspect}"
+        #data here is a solr_document
         add_record
-        index_file_set
       elsif action_type == "remove"
+        #data here is a solr_socument id
         remove_record
-        remove_work_file_set_from_index
       end
-    end
-
-    def self.deindex_work(solr_document, parent_cname)
-      new(solr_document, 'remove', parent_cname).update
     end
 
     private
 
     def add_record
       AccountElevator.switch!(tenant_cname)
+      puts "executing add_record method in Ubiquity:: SharedIndexSolrServiceWrapper with  - #{data.inspect}"
       service = ActiveFedora::SolrService
+        #data here is a solr_socument
       #softCommit first commits to memory then to disk
-      service.add(solr_document, softCommit: true)
+      service.add(data, softCommit: true)
       service.commit
-    end
-
-    def index_file_set
-      AccountElevator.switch!(tenant_cname)
-      if file_sets.present?
-        #this is not an rsolr coonection but it calls it
-        service = ActiveFedora::SolrService
-        file_sets.each do |file_set_doc|
-          service.add(file_set_doc, softCommit: true)
-        end
-        service.commit
-      end
     end
 
     def remove_record
+      puts "executing add_record method in Ubiquity:: SharedIndexSolrServiceWrapper with  - #{data.inspect}"
       AccountElevator.switch!(tenant_cname)
       service = ActiveFedora::SolrService.instance.conn
-      if solr_document.class == Hash
-       #solr_conn.delete_by_id(solr_document['id'])
-        service.delete_by_id(solr_document.with_indifferent_access['id'])
-      else
-        ids = solr_document.map { |hash| hash.with_indifferent_access['id'] }
+      if data.class == Hash
+        puts "hash data for removal #{data.inspect}"
+       #solr_conn.delete_by_id(data['id'])
+      service.delete_by_id(data.with_indifferent_access['id'])
+
+     elsif data.class == Array
+       puts "array of data for removal from shared search #{data.inspect}"
+        #ids = data.map { |hash| hash.with_indifferent_access['id'] }
+        ids = data.map { |id| id if id.class == String}.compact
         #an rsolr method
         service.delete_by_id(ids)
+      else
+        puts "string id for removal from shared search #{data.inspect}"
+        service.delete_by_id(data)
       end
       service.commit
     end
-
-
-
 
   end
 end

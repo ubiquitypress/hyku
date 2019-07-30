e893467e1e6d5dcfc3fdf63400730728eaa3b67d
ashikajith <ashikajith@gmail.com>
2019-07-30 17:36:09 +0100

    Added review and added service for the fetching the datat

--- app/mailers/work_report_mailer.rb
+++ app/mailers/work_report_mailer.rb
@@ -1,5 +1,5 @@
 class WorkReportMailer < ApplicationMailer
-  default from: 'notifications@example.com'
+  default from: ENV['SETTINGS__CONTACT_EMAIL']
 
   def send_report(email, current_cname, email_type)
     @cname = current_cname
@@ -24,42 +24,7 @@ class WorkReportMailer < ApplicationMailer
         }
       )
       sorted_array = query_data.sort_by{|e| [e["human_readable_type_tesim"], e['resource_type_tesim']] }.group_by { |e| e["human_readable_type_tesim"] }.to_h.values.flatten
-      generate_weekly_report(sorted_array)
-    end
-
-    def generate_weekly_report(sorted_array)
-      final_hash = {}
-      sorted_array.group_by { |e| e['resource_type_tesim'] }.each do |key, value|
-        work_with_file_count, work_without_file_count = 0, 0
-        public_records, private_records, embargo, lease = 0,0,0,0
-        institute_records, new_files_added = 0, 0
-        sample_hash = {}
-        value.each do |record_hash|
-          sample_hash[:work_type] = record_hash['human_readable_type_tesim'].first
-          sample_hash[:resource_type] = record_hash['resource_type_tesim'].first
-          if record_hash['hasLease_ssim'].present? && record_hash['lease_history_ssim'].blank?
-            lease += 1
-          elsif record_hash['hasEmbargo_ssim'].present? && record_hash['embargo_history_ssim'].blank?
-            embargo += 1
-          elsif record_hash['visibility_ssi'] == 'authenticated'
-            institute_records += 1
-          elsif record_hash['visibility_ssi'] == 'open'
-            public_records += 1
-          else
-            private_records += 1
-          end
-          if record_hash['file_set_ids_ssim'].present?
-            new_files_added += record_hash['file_set_ids_ssim'].count
-            work_with_file_count += 1
-          else
-            work_without_file_count += 1
-          end
-        end
-        final_hash[key.first] = sample_hash.merge(work_with_file_count: work_with_file_count, work_without_file_count: work_without_file_count,
-                                                  new_files_added: new_files_added, embargo: embargo ,lease: lease ,
-                                                  public_records: public_records ,private_records: private_records, institute_records: institute_records)
-      end
-      final_hash
+      Ubiquity::FetchMailerReportData.new(sorted_array).generate_report
     end
 
     def load_resource_type_yaml
@@ -83,11 +48,11 @@ class WorkReportMailer < ApplicationMailer
     def fetch_subject_based_on(email_type)
       case email_type
       when 'weekly_email_list'
-        "#{Date.today - 7.days} to #{Date.today}"
+        "#{Time.zone.today - 7.days} to #{Time.zone.today}"
       when 'monthly_email_list'
-        "#{Date.today - 1.month} to #{Date.today}"
+        "#{Time.zone.today - 1.month} to #{Time.zone.today}"
       when 'yearly_email_list'
-        "#{Date.today - 1.year} to #{Date.today}"
+        "#{Time.zone.today - 1.year} to #{Time.zone.today}"
       end
     end
 end

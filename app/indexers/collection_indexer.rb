99305cc3e211eeb4cdf647b91fb721aaa259d266
ashikajith <ashikajith@gmail.com>
2019-08-09 10:16:43 +0100

    Added new field for the collection tenant url

--- app/indexers/collection_indexer.rb
+++ app/indexers/collection_indexer.rb
@@ -9,7 +9,7 @@ class CollectionIndexer < Hyrax::CollectionIndexer
   # Uncomment this block if you want to add custom indexing behavior:
   def generate_solr_document
     super.tap do |solr_doc|
-      solr_doc[Solrizer.solr_name('work_tenant_url', :stored_searchable)] = Ubiquity::FetchTenantUrl.new(object).process_url
+      solr_doc[Solrizer.solr_name('collection_tenant_url', :stored_searchable)] = Ubiquity::FetchTenantUrl.new(object).process_url
     end
   end
 end

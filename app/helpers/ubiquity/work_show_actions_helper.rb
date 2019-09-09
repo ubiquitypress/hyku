0563a2c9c8faf9e6425e95db9604e5e2c1bb0598
antonio <antoniodaria@msn.com>
2019-09-09 10:03:25 +0100

    Implemented a dynamic message switch when trying to delete a work with live doi from dashboard/works

--- app/helpers/ubiquity/work_show_actions_helper.rb
+++ app/helpers/ubiquity/work_show_actions_helper.rb
@@ -1,11 +1,18 @@
 module Ubiquity::WorkShowActionsHelper
 
-  def delete_pop_up_message(presenter)
-     if work_status_code(presenter.solr_document.id) == 200
-       link_to "Delete", [main_app, presenter], class: 'btn btn-danger', data: { confirm: "Warning: This #{presenter.human_readable_type} has a live DOI. Deleting the work will cause the DOI to lead to a non-existent page." }, method: :delete
-     else
-       link_to "Delete", [main_app, presenter], class: 'btn btn-danger', data: { confirm: "Delete this #{presenter.human_readable_type}?" }, method: :delete
-     end
+  def delete_pop_up_message(presenter, request_path)
+
+    if (request_path == "/dashboard/my/works") && (work_status_code(presenter.solr_document.id) == 200)
+      link_to " Delete Work", [main_app, presenter], class:'glyphicon glyphicon-trash', data: { confirm: "Warning: This #{presenter.human_readable_type} has a live DOI. Deleting the work will cause the DOI to lead to a non-existent page." }, method: :delete
+    elsif (request_path == "/dashboard/my/works")
+      link_to " Delete Work", [main_app, presenter], class:'glyphicon glyphicon-trash', data: { confirm: "Deleting a work is permanent. Click OK to delete this work, or Cancel to cancel this operation" }, method: :delete
+    elsif work_status_code(presenter.solr_document.id) == 200
+      link_to "Delete", [main_app, presenter], class: 'btn btn-danger', data: { confirm: "Warning: This #{presenter.human_readable_type} has a live DOI. Deleting the work will cause the DOI to lead to a non-existent page." }, method: :delete
+    else
+      link_to "Delete", [main_app, presenter], class: 'btn btn-danger', data: { confirm: "Delete this #{presenter.human_readable_type}?" }, method: :delete
+    end
+
+
   end
 
   private

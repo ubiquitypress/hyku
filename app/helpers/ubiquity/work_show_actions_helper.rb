e26abe8e27cd2e569f57f4def7ce3a7dc4473ce2
antonio <antoniodaria@msn.com>
2019-09-11 15:07:11 +0100

    restyling the font and icon for link to delete from dashboard actions drop-down

--- app/helpers/ubiquity/work_show_actions_helper.rb
+++ app/helpers/ubiquity/work_show_actions_helper.rb
@@ -3,9 +3,9 @@ module Ubiquity::WorkShowActionsHelper
   def delete_pop_up_message(presenter, request_path)
 
     if (request_path == "/dashboard/my/works" || request_path == "/dashboard/works" ) && (work_status_code(presenter.solr_document.id) == 200)
-      link_to " Delete Work", [main_app, presenter], class:'glyphicon glyphicon-trash', data: { confirm: "Warning: This #{presenter.human_readable_type} has a live DOI. Deleting the work will cause the DOI to lead to a non-existent page." }, method: :delete
+      link_to "<i class='glyphicon glyphicon-trash'></i><span> Delete Work</span>".html_safe, [main_app, presenter], data: { confirm: "Warning: This #{presenter.human_readable_type} has a live DOI. Deleting the work will cause the DOI to lead to a non-existent page." }, method: :delete
     elsif (request_path == "/dashboard/my/works" || request_path == "/dashboard/works" )
-      link_to " Delete Work", [main_app, presenter], class:'glyphicon glyphicon-trash', data: { confirm: "Deleting a work is permanent. Click OK to delete this work, or Cancel to cancel this operation" }, method: :delete
+      link_to "<i class='glyphicon glyphicon-trash'></i><span> Delete Work</span>".html_safe, [main_app, presenter], data: { confirm: "Deleting a work is permanent. Click OK to delete this work, or Cancel to cancel this operation" }, method: :delete
     elsif work_status_code(presenter.solr_document.id) == 200
       link_to "Delete", [main_app, presenter], class: 'btn btn-danger', data: { confirm: "Warning: This #{presenter.human_readable_type} has a live DOI. Deleting the work will cause the DOI to lead to a non-existent page." }, method: :delete
     else

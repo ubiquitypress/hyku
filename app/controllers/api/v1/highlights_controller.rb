60aa8cd06b91fef4dc41d392f05d571d8d832028
edward <bauchiroad@gmail.com>
2019-10-28 15:36:01 +0000

    api /highlights endpoints will store individual query result in redis and then store highlights index.json.jbuilder rendered content in redis too

--- app/controllers/api/v1/highlights_controller.rb
+++ app/controllers/api/v1/highlights_controller.rb
@@ -6,25 +6,29 @@ class API::V1::HighlightsController < ActionController::Base
   before_action :set_limit
 
   def index
-    #if nil is reurned, we replace it with "null", JSON.parse can return nil
-    collections = get_collections || "null"
-    @featured_works = get_featured_works || "null"
-    @recent_documents =  get_recent_documents || "null"
-   render json: {explore_collections: JSON.parse(collections), featured_works: JSON.parse(@featured_works), recent_works: JSON.parse(@recent_documents) }
+    @collections = get_collections || []
+    @featured_works = get_featured_works || []
+    @recent_documents =  get_recent_documents || []
+   json = Rails.cache.fetch("multiple/highlights/#{@tenant.try(:cname)}/#{set_last_modified_date}") do
+            render_to_string(:template => 'api/v1/highlights/index.json.jbuilder', locals: {collections: @collections, featured_works: @featured_works,
+                  recent_documents: @recent_documents })
+          end
+
+   render json: json
+
   end
 
   private
 
   def get_collections
     record = CatalogController.new.repository.search(q: "id:*", fq: "has_model_ssim:Collection" , rows: 1, "sort" => "score desc, system_modified_dtsi desc")
-    collection_id = record['response']['docs'].first['id']
+    collection_id = record.dig('response','docs').presence && record.dig('response','docs').first['id']
     last_updated_child  = CatalogController.new.repository.search(q: "member_of_collection_ids_ssim:#{collection_id}", rows: 1, "sort" => "score desc, system_modified_dtsi desc")
 
-    if record.present?
+    if record.dig('response','docs').try(:present?)
       set_cache_key = add_filter_by_class_type_with_pagination_cache_key(record, last_updated_child)
       collections_json  = Rails.cache.fetch(set_cache_key) do
-        @collections  = CatalogController.new.repository.search(q: 'id:*', fq: 'has_model_ssim:Collection', rows: limit, sort: 'system_created_dtsi desc')
-        render_to_string(:template => 'api/v1/collection/index.json.jbuilder', locals: {collections: @collections})
+        CatalogController.new.repository.search(q: 'id:*', fq: 'has_model_ssim:Collection', rows: limit, sort: 'system_created_dtsi desc')
       end
     end
   end
@@ -32,13 +36,13 @@ class API::V1::HighlightsController < ActionController::Base
   def get_recent_documents
     record =  CatalogController.new.repository.search(q: "id:*", fq: models_to_search , rows: 1, "sort" => "score desc, system_modified_dtsi desc")
     total_count = record['response']['numFound']
-    file_ids = record['response']['docs'].first["file_set_ids_ssim"]
-    last_updated_child = CatalogController.new.repository.search(q: "", fq: ["{!terms f=id}#{file_ids.join(',')}"],  rows: 1, "sort" => "score desc, system_modified_dtsi desc")
-    if record.present?
+    file_ids = record && record['response']['docs'].presence && record['response']['docs'].first["file_set_ids_ssim"]
+    new_file_ids = file_ids.present? ? file_ids.join(',') : nil
+    last_updated_child = CatalogController.new.repository.search(q: "", fq: ["{!terms f=id}#{new_file_ids}"],  rows: 1, "sort" => "score desc, system_modified_dtsi desc")
+    if record.dig('response','docs').try(:present?)
       set_cache_key = add_filter_by_class_type_with_pagination_cache_key(record, last_updated_child)
-      works_json =  Rails.cache.fetch(set_cache_key) do
-        @works = CatalogController.new.repository.search(:q=>"id:*", sort: 'system_create_dtsi desc', rows: limit, fq: models_to_search)
-        render_to_string(:template => 'api/v1/work/index.json.jbuilder', locals: {works: @works})
+      Rails.cache.fetch(set_cache_key) do
+        CatalogController.new.repository.search(:q=>"id:*", sort: 'system_create_dtsi desc', rows: limit, fq: models_to_search)
       end
     end
   end
@@ -46,16 +50,17 @@ class API::V1::HighlightsController < ActionController::Base
   def get_featured_works
     featured_works = FeaturedWorkList.new.featured_works
     ids = featured_works.map(&:work_id)
+    joined_ids =  ids.present? ? ids.join(',') : nil
+    record = CatalogController.new.repository.search(q: "", fq: ["{!terms f=id}#{joined_ids}"], rows: 1, "sort" => "score desc, system_modified_dtsi desc")
 
-    record = CatalogController.new.repository.search(q: "", fq: ["{!terms f=id}#{ids.join(',')}"], rows: 1, "sort" => "score desc, system_modified_dtsi desc")
-    file_ids = record['response']['docs'].first["file_set_ids_ssim"]
-    last_updated_child = CatalogController.new.repository.search(q: "", fq: ["{!terms f=id}#{file_ids.join(',')}"],  rows: 1, "sort" => "score desc, system_modified_dtsi desc")
+    file_ids = record && record['response']['docs'].presence && record['response']['docs'].first["file_set_ids_ssim"]
+    new_file_ids = file_ids.present? ? file_ids.join(',') : nil
+    last_updated_child = CatalogController.new.repository.search(q: "", fq: ["{!terms f=id}#{new_file_ids}"],  rows: 1, "sort" => "score desc, system_modified_dtsi desc")
 
-    if record.present?
+    if record.dig('response','docs').try(:present?)
       set_cache_key = add_filter_by_class_type_with_pagination_cache_key(record, last_updated_child)
-      works_json =  Rails.cache.fetch(set_cache_key) do
-        @works = CatalogController.new.repository.search(q: "", fq: ["{!terms f=id}#{ids.join(',')}"], rows: limit)
-        render_to_string(:template => 'api/v1/work/index.json.jbuilder', locals: {works: @works})
+      Rails.cache.fetch(set_cache_key) do
+        CatalogController.new.repository.search(q: "", fq: ["{!terms f=id}#{ids.join(',')}"], rows: limit)
       end
     end
 
@@ -65,6 +70,13 @@ class API::V1::HighlightsController < ActionController::Base
     if params[:per_page].blank?
       @limit = default_limit_for_hightlights
     end
+  end
+
+  def set_last_modified_date
+    collection_records = get_collections.presence && get_collections['response']['docs'].try(:last).dig('system_modified_dtsi')
+    featured_records = get_featured_works.presence && get_featured_works['response']['docs'].try(:last).dig('system_modified_dtsi')
+    recent_records = get_recent_documents.presence && get_recent_documents['response']['docs'].try(:last).dig('system_modified_dtsi')
+    dates_array = [collection_records, featured_records, recent_records].compact.max
   end
 
 end
--- app/views/api/v1/highlights/index.json.jbuilder
+++ app/views/api/v1/highlights/index.json.jbuilder
@@ -1,11 +1,28 @@
-json.explore_collections do
-  json.partial! 'api/v1/collection/collection', collection: @collections['response']['docs'], as: :single_collection
+
+collection_list = @collections.presence && @collections['response']['docs']
+if collection_list.present?
+  json.explore_collections do
+    json.partial! 'api/v1/collection/collection', collection: @collections['response']['docs'], as: :single_collection
+  end
+else
+  json.explore_collections  nil
 end
 
-json.featured_works do
-  json.partial! 'api/v1/work/work', collection: @featured_works['response']['docs'], as: :work
+featured = @featured_works.presence && featured_works['response']['docs']
+
+if featured.present?
+  json.featured_works do
+    json.partial! 'api/v1/work/work', collection: @featured_works['response']['docs'], as: :work
+  end
+else
+  json.featured_works  nil
 end
 
-json.recent_works do
-  json.partial! 'api/v1/work/work', collection: @recent_documents['response']['docs'], as: :work
+recent = @recent_documents.presence && @recent_documents['response']['docs']
+if recent.present?
+  json.recent_works do
+    json.partial! 'api/v1/work/work', collection: @recent_documents['response']['docs'], as: :work
+  end
+else
+  json.recent_works nil
 end

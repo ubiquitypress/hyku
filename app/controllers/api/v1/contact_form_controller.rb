09756b43f64b0ab87cf6484ebd5b099740fa83b0
edward <bauchiroad@gmail.com>
2019-11-01 15:10:56 +0000

    allow contact form to be submitted via api call

---
+++ app/controllers/api/v1/contact_form_controller.rb
@@ -0,0 +1,21 @@
+class API::V1::ContactFormController < ActionController::Base
+
+  #defines :limit, :default_limit, :models_to_search, :switche_tenant
+  include Ubiquity::ApiControllerUtilityMethods
+
+  def create
+    hash =  {category: params[:category], name: params[:name], email: params[:email], subject: params[:subject],
+           message: params[:message], contact_method: params[:contact_method]}
+     @contact_form =  Hyrax::ContactForm.new(hash)
+     headers = {subject: "#{Hyrax.config.subject_prefix} #{ @contact_form.subject}", to: Hyrax.config.contact_email,
+                 from: @contact_form.email}
+     @contact_form.headers = headers
+     if @contact_form.spam?
+       render json: {code: 422,  status: :spam_not_sent}
+     else
+       Hyrax::ContactMailer.contact(@contact_form, @tenant).deliver_now
+       render json: {code: 201,  status: :created}
+     end
+  end
+
+end

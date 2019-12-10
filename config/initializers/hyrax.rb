b25c36930f1fc20b2711460044549ca800d84dff
antonio <antoniodaria@msn.com>
2019-12-10 16:58:31 +0000

    removing configuration and resource type to eliminate conflicts

--- config/initializers/hyrax.rb
+++ config/initializers/hyrax.rb
@@ -30,8 +30,7 @@ Hyrax.config do |config|
   config.register_curation_concern :text_work
   # Injected via `rails g hyrax:work NewsClipping`
   config.register_curation_concern :news_clipping
-  # Injected via `rails g hyrax:work ImageWork`
-  config.register_curation_concern :image_work
+
 
 
 

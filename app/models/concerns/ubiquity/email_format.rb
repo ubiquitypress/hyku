b20e9db7be481ee0261edee8cad8c3147a12187c
BertZZ <bertie.wooles@gmail.com>
2020-03-09 13:34:58 +0000

    Makes change to concern pattern

--- app/models/concerns/ubiquity/email_format.rb
+++ app/models/concerns/ubiquity/email_format.rb
@@ -4,21 +4,20 @@ module Ubiquity
 
     included do
       validate :must_have_valid_email_format
+    end
 
-      def get_tenant_work_settings
-        json_data = ENV['TENANTS_WORK_SETTINGS']
-          if json_data.present? && json_data.class == String
-          JSON.parse(json_data)
-        end
+    def get_tenant_work_settings
+      json_data = ENV['TENANTS_WORK_SETTINGS']
+        if json_data.present? && json_data.class == String
+        JSON.parse(json_data)
       end
+    end
 
-      def must_have_valid_email_format
-        format = get_tenant_work_settings['email_format']
-        if format.present?
-          errors.add(:email, "Email must contain #{format}") unless email.include? format
-        end
+    def must_have_valid_email_format
+      format = get_tenant_work_settings['email_format']
+      if format.present?
+        errors.add(:email, "Email must contain #{format}") unless email.include? format
       end
-
     end
   end
 end

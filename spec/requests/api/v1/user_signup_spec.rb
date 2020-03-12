72d419e28834f41091dc9c2a65539cc9139d6d7b
edward <bauchiroad@gmail.com>
2020-03-12 18:48:57 +0000

    api user signup test

---
+++ spec/requests/api/v1/user_signup_spec.rb
@@ -0,0 +1,25 @@
+RSpec.describe API::V1::RegistrationsController, type: :request do
+  describe 'POST #signup' do
+    let!(:account) { create(:account) }
+    json_data = ENV['TENANTS_WORK_SETTINGS']
+    if json_data.present? && json_data.class == String
+      data = JSON.parse(json_data)
+    end
+
+    before do
+      allow(AccountElevator).to receive(:switch!).with(account.cname)
+      allow(AdminSet).to receive(:all).and_return([])
+    end
+
+    it 'creates a user when given the proper params' do
+        post api_v1_user_signup_url(tenant_id: account.tenant), params: {
+          email: "test.test#{data['email_format']}",
+          password: 'Potato123!',
+          password_confirmation: 'Potato123!'
+        }
+       parsed_response = JSON.parse(response.body)
+      expect(parsed_response['email']).to eq("test.test#{data['email_format']}")
+    end
+
+  end
+end

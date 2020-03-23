3f22bc297046c3d0a7aea785af3f56101111d6ff
BertZZ <bertie.wooles@gmail.com>
2020-03-23 17:58:09 +0000

    Subs front end url with api url

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -123,4 +123,12 @@ module ApplicationHelper
     @record =  Account.where(cname: request.host, data: {'is_parent': 'true'}).first
   end
 
+  def get_tenant_work_settings
+  json_data = ENV['TENANTS_WORK_SETTINGS']
+    if json_data.present? && json_data.class == String
+    JSON.parse(json_data)
+  end
+end
+
+
 end

46a00bb576cea1855b72d83a44ccbb801d44065c
BertZZ <bertie.wooles@gmail.com>
2020-03-23 17:58:31 +0000

    Subs api url with front end url

--- app/services/ubiquity/fetch_tenant_url.rb
+++ app/services/ubiquity/fetch_tenant_url.rb
@@ -1,16 +1,19 @@
 module Ubiquity
   class FetchTenantUrl
+    include ApplicationHelper
 
     def initialize(object)
       @account_cname = object.try(:account_cname)
       @id = object.try(:id)
       @work_type = object.try(:has_model).try(:first).to_s
       @is_collection = object.collection?
+      @settings = get_tenant_work_settings
     end
 
     def process_url
       return nil if @account_cname.nil?
       work_type = @work_type.tableize
+      @account_cname = @account_cname.gsub(@settings['sub_cname']['api'], @settings['sub_cname']['live']) if @account_cname == @settings['sub_cname']['api']
       if @account_cname.split('.').include? 'localhost'
         @is_collection ? "http://#{@account_cname}:3000/#{work_type}/#{@id}" : "http://#{@account_cname}:3000/concern/#{work_type}/#{@id}"
       else

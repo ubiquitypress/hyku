119c6173f72ceb5baf5bf2c12770ea715a55984e
BertZZ <bertie.wooles@gmail.com>
2020-03-24 15:53:10 +0000

    Small formatting change

--- app/services/ubiquity/fetch_tenant_url.rb
+++ app/services/ubiquity/fetch_tenant_url.rb
@@ -13,11 +13,13 @@ module Ubiquity
       return nil if @account_cname.nil?
       work_type = @work_type.tableize
       @account_cname = @account_cname.gsub(@settings['sub_cname']['api'], @settings['sub_cname']['live']) if @account_cname == @settings['sub_cname']['api'] && @settings['sub_cname']['api'].present?
+
       if @account_cname.split('.').include? 'localhost'
         @is_collection ? "http://#{@account_cname}:3000/#{work_type}/#{@id}" : "http://#{@account_cname}:3000/concern/#{work_type}/#{@id}"
       else
-      @is_collection ? "https://#{@account_cname}/collection/#{@id}" : "https://#{@account_cname}/work/#{@id}"
+        @is_collection ? "https://#{@account_cname}/collection/#{@id}" : "https://#{@account_cname}/work/#{@id}"
       end
+      
     end
   end
 end

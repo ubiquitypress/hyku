f94028b41a3dc891461e8d20a0e0d6869260dcf6
BertZZ <bertie.wooles@gmail.com>
2020-03-26 14:39:55 +0000

    Guards against lack of settings and allows more complex hash structure

--- app/services/ubiquity/fetch_tenant_url.rb
+++ app/services/ubiquity/fetch_tenant_url.rb
@@ -12,14 +12,18 @@ module Ubiquity
     def process_url
       return nil if @account_cname.nil?
       work_type = @work_type.tableize
-      @account_cname = @account_cname.gsub(@settings['sub_cname']['api'], @settings['sub_cname']['live']) if @account_cname == @settings['sub_cname']['api'] && @settings['sub_cname']['api'].present?
+
+      if @settings['sub_cname'].present?
+        @settings['sub_cname'].each do |tenent, settings_hash|
+          @account_cname = @account_cname.gsub(settings_hash['api'], settings_hash['live']) if @account_cname == settings_hash['api'] && settings_hash['api'].present?
+        end
+      end
 
       if @account_cname.split('.').include? 'localhost'
         @is_collection ? "http://#{@account_cname}:3000/#{work_type}/#{@id}" : "http://#{@account_cname}:3000/concern/#{work_type}/#{@id}"
       else
         @is_collection ? "https://#{@account_cname}/collection/#{@id}" : "https://#{@account_cname}/work/#{@id}"
       end
-      
     end
   end
 end

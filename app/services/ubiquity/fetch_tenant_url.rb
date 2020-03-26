e36ab74d9109a0ee6bab061212313a6fde3f5150
BertZZ <bertie.wooles@gmail.com>
2020-03-26 15:28:04 +0000

    Breaks loop once cname has been changed

--- app/services/ubiquity/fetch_tenant_url.rb
+++ app/services/ubiquity/fetch_tenant_url.rb
@@ -14,8 +14,12 @@ module Ubiquity
       work_type = @work_type.tableize
 
       if @settings['sub_cname'].present?
-        @settings['sub_cname'].each do |tenent, settings_hash|
+        @settings['sub_cname'].each do |tenant, settings_hash|
+          puts "STARTING LOOP"
           @account_cname = @account_cname.gsub(settings_hash['api'], settings_hash['live']) if @account_cname == settings_hash['api'] && settings_hash['api'].present?
+          puts "Current Cname is: #{@account_cname}"
+          puts "Current api setting is #{settings_hash['api']}"
+          break if @account_cname == settings_hash['live']
         end
       end
 

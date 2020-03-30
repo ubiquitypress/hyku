e02e9ffe684d4103a865d5d1284a4f01196b30fd
edward <bauchiroad@gmail.com>
2020-03-30 13:27:15 +0100

    use TENANT_SETTINGS for pacific instead of TENANT_WORK_SETTINGS when generating url for oai and skipping fedora association

--- app/services/ubiquity/fetch_tenant_url.rb
+++ app/services/ubiquity/fetch_tenant_url.rb
@@ -1,3 +1,9 @@
+# How to use
+#change the cname as needed and copy the id of any work in the records of that cname
+# work = Article.new(id: '2c0ec6fe-f47b-4e92-93d2-db86e1896b5c', account_cname: "bl.oar.uk", title: ['test'])
+# f = Ubiquity::FetchTenantUrl.new(work)
+# f.process_url
+#
 module Ubiquity
   class FetchTenantUrl
 
@@ -6,24 +12,25 @@ module Ubiquity
       @id = object.try(:id)
       @work_type = object.try(:has_model).try(:first).to_s
       @is_collection = object.collection?
-      @settings = Ubiquity::ParseTenantWorkSettings.new(@account_cname).tenant_work_settings_hash
+      @settings = Ubiquity::ParseTenantWorkSettings.new(@account_cname)
+      @settings_hash = @settings.per_account_tenant_settings_hash
+      @subdomain = @settings.get_tenant_subdomain
     end
 
     def process_url
       return nil if @account_cname.nil?
       work_type = @work_type.tableize
 
-      if @settings && @settings['per_account_settings'].present?
-        @settings['per_account_settings'].each do |tenant, settings_hash|
-          @account_cname = @account_cname.gsub(settings_hash['api'], settings_hash['live']) if @account_cname == settings_hash['api'] && settings_hash['api'].present?
-          break if @account_cname == settings_hash['live']
-        end
+      if @settings && @settings_hash.present? && @settings_hash[@subdomain].present? && @settings_hash[@subdomain]['live'].present?
+        @account_cname = @settings_hash[@subdomain]['live']
       end
 
       if @account_cname.split('.').include? 'localhost'
         @is_collection ? "http://#{@account_cname}:3000/#{work_type}/#{@id}" : "http://#{@account_cname}:3000/concern/#{work_type}/#{@id}"
-      else
+      elsif @settings_hash.present? && @settings_hash[@subdomain].present? && @settings_hash[@subdomain]['live'].present?
         @is_collection ? "https://#{@account_cname}/collection/#{@id}" : "https://#{@account_cname}/work/#{@id}"
+      else
+        @is_collection ? "https://#{@account_cname}/#{work_type}/#{@id}" : "https://#{@account_cname}/concern/#{work_type}/#{@id}"
       end
     end
   end

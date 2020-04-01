3b2efd19f86914f277b8c5658e933298fa2bf697
BertZZ <bertie.wooles@gmail.com>
2020-04-01 14:58:15 +0100

    Adds test for URL

--- spec/services/fetch_tenant_url_spec.rb
+++ spec/services/fetch_tenant_url_spec.rb
@@ -13,4 +13,11 @@ RSpec.describe Ubiquity::FetchTenantUrl do
     f = Ubiquity::FetchTenantUrl.new(work)
     expect { f.process_url }.to_not change { f.instance_variable_get(:@account_cname) }
   end
+
+  it 'generates a new URL with the live cname' do
+    account = FactoryGirl.create(:account, cname: "mola")
+    work = GenericWork.new("account_cname": account.cname)
+    f = Ubiquity::FetchTenantUrl.new(work)
+    expect(f.process_url).to include("mola.iro.bl.uk")
+  end
 end

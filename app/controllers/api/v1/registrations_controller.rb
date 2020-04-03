c2906601b1708a2fd2d98026d897f16fea901f54
BertZZ <bertie.wooles@gmail.com>
2020-04-03 15:29:21 +0100

    Changes render on registration from API

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -5,13 +5,13 @@ class API::V1::RegistrationsController <  API::V1::ApiBaseController
     AccountElevator.switch!(current_account.cname)
     user = User.new(email: api_user_params['email'], password: api_user_params['password'],
               password_confirmation: api_user_params['password_confirmation'])
-              
+
     if user.valid? && user.save
       token = payload(user)
       set_response_cookie(token)
       participants = adminset_permissions(user)
       user_type = user_roles(user)
-      render json: user.slice(:email).merge({participants: participants, type: user_type })
+      render text: "Please check your email at #{user.slice(:email)} to complete your registration", status: 200
     else
       message = user.errors.messages
       error_object = Ubiquity::ApiError::NotFound.new(status: 401, code: 'Invalid credentials', message: message)

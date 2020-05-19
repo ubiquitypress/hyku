876f7307938543aab516752d0894f6ba25ecb429
edward <bauchiroad@gmail.com>
2020-05-19 11:23:29 +0100

    remove duplicate works when displaying a collection

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -123,8 +123,9 @@ module ApplicationHelper
     @record =  Account.where(cname: request.host, data: {'is_parent': 'true'}).first
   end
 
-  def check_should_not_use_fedora_association
-    settings_parser_class = Ubiquity::ParseTenantWorkSettings.new(request.original_url)
+  def check_should_not_use_fedora_association(url_or_account_cname = nil)
+    new_url = url_or_account_cname || request.try(:original_url)
+    settings_parser_class = Ubiquity::ParseTenantWorkSettings.new(new_url)
     tenant_work_settings_hash = settings_parser_class.per_account_tenant_settings_hash
     subdomain = settings_parser_class.get_tenant_subdomain
     tenant_work_settings_hash && tenant_work_settings_hash[subdomain] && tenant_work_settings_hash[subdomain]["turn_off_fedora_collection_work_association"]

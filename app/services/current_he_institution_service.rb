9a982bbac9da86f6e40d785be2ccd20a68ab391d
BertZZ <bertie.wooles@gmail.com>
2020-07-17 16:00:00 +0100

    Adds first implmentation of autoselection of ROR and ISNI's and adds mappings for avalible ROR's and ISNI's provided by the BL

--- app/services/current_he_institution_service.rb
+++ app/services/current_he_institution_service.rb
@@ -5,10 +5,30 @@ class CurrentHeInstitutionService < Hyrax::QaSelectService
   self.authority = Qa::Authorities::Local.subauthority_for('current_he_institution')
 
   def self.select_active_options
-    active_elements.map { |e| [e[:label], e[:id]] }
+    all_local.map { |e| [e[:label], e[:id]] }
   end
 
-  def self.active_elements
-    authority.all.select { |e| e.fetch('active') }
+  def self.select_active_options_isni
+    all_local.map { |e| e[:isni] }
   end
+
+  def self.select_active_options_ror
+    all_local.map { |e| e[:ror] }
+  end
+
+  def self.subauthority_filename_local
+    File.join(Qa::Authorities::Local.subauthorities_path, "current_he_institution.yml")
+  end
+
+  def self.terms_local
+    subauthority_hash = YAML.load(File.read(subauthority_filename_local))
+    terms = subauthority_hash.with_indifferent_access.fetch(:terms, [])
+  end
+
+  def self.all_local
+    terms_local.map do |res|
+      { id: res[:id], label: res[:term], isni: res[:isni], ror: res[:ror], active: res.fetch(:active, true) }.with_indifferent_access
+    end
+  end
+
 end

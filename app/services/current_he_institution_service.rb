247d269fc2e117ac8ea9d4c0df71f48977e0c0b5
BertZZ <bertie.wooles@gmail.com>
2020-07-22 13:48:42 +0100

    Cleans Up method names and puts statements

--- app/services/current_he_institution_service.rb
+++ app/services/current_he_institution_service.rb
@@ -5,28 +5,28 @@ class CurrentHeInstitutionService < Hyrax::QaSelectService
   self.authority = Qa::Authorities::Local.subauthority_for('current_he_institution')
 
   def self.select_active_options
-    all_local.map { |e| [e[:label], e[:id]] }
+    all_options_hash.map { |e| [e[:label], e[:id]] }
   end
 
   def self.select_active_options_isni
-    all_local.map { |e| e[:isni] }
+    all_options_hash.map { |e| e[:isni] }
   end
 
   def self.select_active_options_ror
-    all_local.map { |e| e[:ror] }
+    all_options_hash.map { |e| e[:ror] }
   end
 
-  def self.subauthority_filename_local
+  def self.subauthority_filename
     File.join(Qa::Authorities::Local.subauthorities_path, "current_he_institution.yml")
   end
 
-  def self.terms_local
-    subauthority_hash = YAML.load(File.read(subauthority_filename_local))
+  def self.terms
+    subauthority_hash = YAML.load(File.read(subauthority_filename))
     terms = subauthority_hash.with_indifferent_access.fetch(:terms, [])
   end
 
-  def self.all_local
-    terms_local.map do |res|
+  def self.all_options_hash
+    terms.map do |res|
       { id: res[:id], label: res[:term], isni: res[:isni], ror: res[:ror], active: res.fetch(:active, true) }.with_indifferent_access
     end
   end

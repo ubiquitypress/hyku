e7b0244485b527111a4820f5f93a3b713314a018
ashikajith <ashikajith@gmail.com>
2020-08-03 01:42:23 +0100

    Fixed the js issue in adding funder project ref

--- app/assets/javascripts/ubiquity/funder_awards.js
+++ app/assets/javascripts/ubiquity/funder_awards.js
@@ -1,20 +1,19 @@
 $(document).on("turbolinks:load", function(){
-    var add_button = $(".add_funder_awards_field_button");
-    var funder_awards_wrapper = $('.funder_awards_input_fields_wrap')
+	var add_button = $(".add_funder_awards_field_button");
+	var funder_awards_wrapper = $('ul.funder_awards_input_fields_wrap')
 
 	$(add_button).click(function(e){ //on add input button click
-    e.preventDefault();
-    // Fetch and clone last funder award text field
-    cloneUbiDiv = $('.ubiquity_funder_awards :last').clone();
-    cloneUbiDiv.find('input').val('');
-    $('.ubiquity_funder_awards :last').after(cloneUbiDiv)
-    $('.ubiquity_funder_awards :last').append('<a href="#" class="remove_funder_awards_field">Remove</a></div>')
+		e.preventDefault();
+		// Fetch and clone last funder award text field
+		cloneElement = funder_awards_wrapper.find('li:last').clone();
+		cloneElement.find('input').val('');
+		cloneElement.appendTo(funder_awards_wrapper).append('<a href="#" class="remove_funder_awards_field">Remove</a>');
 	});
 
 	$(funder_awards_wrapper).on("click",".remove_funder_awards_field", function(e){ //user click on remove text
     e.preventDefault();
-    if ($(".ubiquity_funder_awards").length > 1 ) {
-      $(this).parent('div').remove();
+    if ($("ul.funder_awards_input_fields_wrap li").length > 1 ) {
+      $(this).closest('li').remove();
     }
 	})
 });

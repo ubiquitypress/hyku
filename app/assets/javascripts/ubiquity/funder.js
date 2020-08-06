2e54a4c6cede49460038710eeca9dd163077263f
ashikajith <ashikajith@gmail.com>
2020-08-06 01:47:39 +0100

    Added div and fixed the js code for awards

--- app/assets/javascripts/ubiquity/funder.js
+++ app/assets/javascripts/ubiquity/funder.js
@@ -1,33 +1,33 @@
 // add linked fields
 $(document).on("turbolinks:load", function(){
   return $("body").on("click", ".add_funder", function(event){
-      event.preventDefault();
-      var ubiquityFunderClass = $(this).attr('data-addUbiquityFunder');
-      cloneUbiDiv = $(this).parent('div' + ubiquityFunderClass + ':last').clone();
+    event.preventDefault();
+    var ubiquityFunderClass = $(this).attr('data-addUbiquityFunder');
+    cloneUbiDiv = $(this).parent('div' + ubiquityFunderClass + ':last').clone();
 
-     _this = this;
-      cloneUbiDiv.find('input').val('');
+    _this = this;
+    cloneUbiDiv.find('input').val('');
 
-      //increment hidden_field counter after cloning
-      var lastInputCount = $('.ubiquity-funder-score:last').val();
-      var hiddenInput = $(cloneUbiDiv).find('.ubiquity-funder-score');
-      hiddenInput.val(parseInt(lastInputCount) + 1);
-      $(ubiquityFunderClass +  ':last').after(cloneUbiDiv)
+    //increment hidden_field counter after cloning
+    var lastInputCount = $('.ubiquity-funder-score:last').val();
+    var hiddenInput = $(cloneUbiDiv).find('.ubiquity-funder-score');
+    hiddenInput.val(parseInt(lastInputCount) + 1);
+    $(ubiquityFunderClass +  ':last').after(cloneUbiDiv)
   });
 });
 
 //remove linked fields
 $(document).on("turbolinks:load", function(){
   return $("body").on("click", ".remove_funder", function(event){
-      event.preventDefault();
-      var ubiquityFunderClass = $(this).attr('data-removeUbiquityFunder');
-      _this = this;
-      removeubiquityFunder(_this, ubiquityFunderClass);
+    event.preventDefault();
+    var ubiquityFunderClass = $(this).attr('data-removeUbiquityFunder');
+    _this = this;
+    removeubiquityFunder(_this, ubiquityFunderClass);
   });
 });
 
 function removeubiquityFunder(self, funderDiv) {
   if ($(".ubiquity-meta-funder").length > 1 ) {
-      $(self).parent('div' + funderDiv).remove();
+    $(self).parent('div' + funderDiv).remove();
   }
 }

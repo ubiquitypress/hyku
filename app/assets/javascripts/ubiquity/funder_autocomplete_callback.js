d8ccc5e8c36f56b3ae11c7ae818c27a685eb5504
ashikajith <ashikajith@gmail.com>
2020-08-11 09:10:56 +0100

    Removed the logs and renamed the funder id variable in js

--- app/assets/javascripts/ubiquity/funder_autocomplete_callback.js
+++ app/assets/javascripts/ubiquity/funder_autocomplete_callback.js
@@ -1,15 +1,14 @@
 $(document).on("turbolinks:load", function(){
   $('.ubiquity_funder_name').autocomplete({
     select: function(ui, result) {
-      console.log('insided select call back')
-      console.log(result.item.id)
       closest_div = $('.ubiquity_funder_name').closest('div')
+      closest_div.find('.ubiquity_funder_doi').val(result.item.uri)
       fetchFunderFieldData(result.item.id, closest_div)
     }
   });
 });
 
-function fetchFunderFieldData(doi_id, closest_div) {
+function fetchFunderFieldData(funder_id, closest_div) {
   var host = window.document.location.host;
   var protocol = window.document.location.protocol;
   var fullHost = protocol + '//' + host + '/available_ubiquity_titles/call_funder_api';
@@ -17,11 +16,10 @@ function fetchFunderFieldData(doi_id, closest_div) {
   $.ajax({
     url: fullHost,
     type: "POST",
-    data: {"doi_id": doi_id},
+    data: {"funder_id": funder_id},
     success: function(result){
-      console.log(result.data.funder_ror);
       if (result.data.error  === undefined) {
-        closest_div.find('.ubiquity_funder_doi').val(result.data.funder_ror)
+        closest_div.find('.ubiquity_funder_ror').val(result.data.funder_ror)
         closest_div.find('.ubiquity_funder_isni').val(result.data.funder_isni)
         $.each(result.data.funder_awards , function(index, val) {
           console.log(index, val);
--- app/controllers/available_ubiquity_titles_controller.rb
+++ app/controllers/available_ubiquity_titles_controller.rb
@@ -18,7 +18,7 @@ class AvailableUbiquityTitlesController < ApplicationController
   end
 
   def call_funder_api
-    response_data = Ubiquity::FunderApiData.new(params['doi_id']).fetch_record
+    response_data = Ubiquity::FunderApiData.new(params['funder_id']).fetch_record
     render json: { 'data': response_data }
   end
 end
